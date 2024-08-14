b21ec84664fa9030694ebac2a0c0685e129ae996
unknown <abhijitrout520@gmail.com>
2024-08-14 05:32:32 +0530

    issue 13

--- public/python/AudioDownload.py
+++ public/python/AudioDownload.py
@@ -104,6 +104,8 @@ else:
 
     service = authentiate_google_drive()
     url = link
+    print(link)
+    sys.stdout.flush()
     file_path, file_name, response_content = download_file(url)
     upload_to_google_drive(service, file_path, file_name, folder_id='1Y_3XGo2z6miI-O9j_U9H0vEN_ea9uQ_k')
 
